3c5f1afec4189c3b275ada18734d78763eebe3dd
orson <e0176881@u.nus.edu>
2021-10-31 02:34:20 +0800

    cleanup

--- api.js
+++ api.js
@@ -1,31 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const multer = require('multer');
 const app = express();
-const cors = require('cors');
-const { httpLogger } = require('./middlewares');
-const { logger } = require('./utils');
 const port = process.env.PORT || 3001;
-var token = '';
-const secret = 'supersecret';
-const path = require('path');
-const { randomInt } = require('crypto');
-
-var productTypeDict = {
-	'127': '',
-	'070': '',
-	'291': '',
-	'969': '',
-	'555': '',
-};
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-const upload = multer();
-app.use(cors());
-app.use(httpLogger);
-// Register the home route that displays a welcome message
-// This route can be accessed without a token
 
   message = 
 ["Logic will get you from A to B. Imagination will take you everywhere.", 
@@ -38,7 +13,7 @@ app.use('/', express.static(__dirname + '/test'));
  
 app.get('/getMessage', function (req, res) {
 	randomNumber = Math.floor(Math.random() * message.length);
-	res.send(JSON.stringify({msg:message[randomNumber]}));
+	res.json({msg:message[randomNumber]});
 });
 
 app.listen(port, () => {
